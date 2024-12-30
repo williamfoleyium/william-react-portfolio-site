cf1c9029266a1ad4c7b19f4b61dbc7e9336b82ee
WilliamFoley <CaptainTrips1500@proton.me>
2024-12-30 18:58:54 -0500

    completed nav headers

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -47,18 +47,42 @@ export default class function Nav() {
 
     const renderNavLink = (to, imgSrc, altText, navClass) => {
       const isCurrent = isCurrentPage(navClass);
-      const linkClass = isCurrent ? "nav-link current" : "nav-link"
-    }
+      const linkClass = isCurrent ? "nav-link current" : "nav-link";
 
-  render() {
+      return (
+        <Link to={to} className={linkClass}>
+          <img src={imgSrc} alt={altText}/>
+          {isCurrent && <h1 className="page-title">{pageTitle}</h1>}
+        </Link>
+      );
+    };
+
     return (
-       <div>
-        <img src={astronautHelmet} />
-        <img src={deadEye} />
-        <img src={stack} />
-        <img src={envelope} />
-       </div>
-
+      <nav className={`nav ${navPositionClass}`}>
+        {renderNavLink(
+          "/",
+          astronautHelmet,
+          "astronaut helmet icon",
+          "nav-about"
+        )}
+        {renderNavLink(
+          "/skills",
+          deadEye,
+          "deadEye  icon",
+          "nav-skills"
+        )}
+        {renderNavLink(
+          "/projects",
+          stack,
+          "stack  icon",
+          "nav-projects"
+        )}
+        {renderNavLink(
+          "/contact",
+          envelope,
+          "envelope  icon",
+          "nav-contact"
+        )}
+      </nav>
     );
-  }
 }
