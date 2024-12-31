afdd60f4951dbce2319242cb9c916dc0a36ad10e
WilliamFoley <CaptainTrips1500@proton.me>
2024-12-31 13:23:07 -0500

    created about files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./nav/Nav.js"
-
+import About from "./about/About"
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Nav />
-    </>
+      <Routes>
+        <Route path="/" element={About}/>
+      </Routes>
+    </Router>
   );
 }
 
