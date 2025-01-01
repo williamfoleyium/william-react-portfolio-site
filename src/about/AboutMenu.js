980050276850f8a495dd7cef811d333a91ebc73b
WilliamFoley <CaptainTrips1500@proton.me>
2025-01-01 16:22:25 -0500

    added aboutmenu and avatar files. updated about page

---
+++ src/about/AboutMenu.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class AboutMenu extends Component {
+  render() {
+    return (
+      <div>AboutMenu</div>
+    )
+  }
+}
---
+++ src/avatar/Avatar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import avatarImage from "../assets/avatar-image.png";
+import "../styles/avatar.css";
+
+export default class Avatar extends Component {
+  render() {
+    return (
+      <div>Avatar</div>
+    )
+  }
+}
