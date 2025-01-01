3433c5bf9e3d99882e3cf5954c21bf4efe4e88e0
WilliamFoley <CaptainTrips1500@proton.me>
2025-01-01 16:30:47 -0500

    updated avatar page

--- src/avatar/Avatar.js
+++ src/avatar/Avatar.js
@@ -3,10 +3,20 @@ import PropTypes from "prop-types";
 import avatarImage from "../assets/avatar-image.png";
 import "../styles/avatar.css";
 
-export default class Avatar extends Component {
-  render() {
-    return (
-      <div>Avatar</div>
-    )
-  }
-}
+const Avatar = ({ page }) => {
+  const avatarClass = `avatar ${page}`;
+  const spanClass = `shadow-overlay-${page}`;
+
+  return (
+    <>
+    <span className={spanClass}></span>
+    <img src={avatarImage} className={avatarClass} alt='avatar' />
+    </>
+  );
+};
+
+Avatar.propTypes = {
+    page: PropTypes.string.isRequired,
+};
+
+export default Avatar;
