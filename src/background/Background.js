c228f7dbac3f550e3b1e0a76c77ad74e316a1fa6
WilliamFoley <CaptainTrips1500@proton.me>
2025-01-01 13:51:43 -0500

    created background

---
+++ src/background/Background.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/background.css";
+import video from "../assets/bg-universe.mp4";
+import fallbackImage from "../assets/fallback-image.png"; 
+
+
+const Background = () => {
+    return (
+        <>
+            <div className="shadow-overlay"></div>
+            <video playInLine autoPlay muted loop preload="auto" id="bg" poster={fallbackImage}>
+                <source src={video} type="video?mp4"></source>
+            </video>
+        </>
+    );
+};
+
+export default Background;
