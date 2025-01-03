509b81af1a38086edffc5259878924876f04cb71
WilliamFoley <CaptainTrips1500@proton.me>
2025-01-03 18:48:14 -0500

    updated skills files

--- src/skills/SkillsMenu.js
+++ src/skills/SkillsMenu.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 import "../styles/skillsMenu.css";
+import classNames from 'classnames';
+import skills from "./skillsData";
+import frontendIcon from "../assets/eagle-emblem.png";
+import backendIcon from "../assets/hawk-emblem.png";
 
 export default class SkillsMenu extends Component {
-  render() {
-    return (
-      <div>SkillsMenu</div>
-    )
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeMenuItem: 1,
+    };
   }
+
+  handleMenuItemClick = (menuItem) => {
+    this.setState({
+      activeMenuItem: menuItem,
+    });
+  };
+
+  renderContent(skills) {
+    return skills.map((skills, index) => (
+      <div key={index} className={`skill-sub-container-${this.state.activeMenuItem}`}>
+        <h3>{skills.title}</h3>
+        <div className="level-container">
+         {[...Array(6)].map(__, i) => {
+          <div key={i} className={`level-point ${i < skills.level ? "filled" : 'unfilled'}`}></div>
+        }} 
+        </div>
+      </div>
+    ))
+
+  }
+      
+
+
 }
