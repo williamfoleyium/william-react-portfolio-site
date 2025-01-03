c83db4ef9f441368950f391ec102e170324f8313
WilliamFoley <CaptainTrips1500@proton.me>
2025-01-03 16:52:57 -0500

    updated about menu files

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,70 @@
+const subheadingsData = {
+    1: [
+        {
+            title: "About Me",
+            content: (
+                <>
+                    <p>
+                        [INSERT ABOUT ME TEXT]
+                    </p>
+                </>
+            ),
+        },
+    ],
+    2: [
+        {
+            title: "2016 Masters Degree",
+            content: (
+                <>
+                    <p>
+                        [INSERT ABOUT ME TEXT]
+                    </p>
+                </>
+            ),
+        },
+        {
+            title: "2014 Bachelors Degree",
+            content: (
+                <>
+                    <p>
+                        [INSERT ABOUT ME TEXT]
+                    </p>
+                </>
+            ),
+        },
+    ],
+    3: [
+        {
+            title: "2022 Front End Developer",
+            content: (
+                <>
+                    <p>
+                        [INSERT ABOUT ME TEXT]
+                    </p>
+                </>
+            ),
+        },
+        {
+            title: "2020 UX Designer",
+            content: (
+                <>
+                    <p>
+                        [INSERT ABOUT ME TEXT]
+                    </p>
+                </>
+            ),
+        },
+        {
+            title: "2017 IT Consultant",
+            content: (
+                <>
+                    <p>
+                        [INSERT ABOUT ME TEXT]
+                    </p>
+                </>
+            ),
+        },
+    ],
+}
+
+export default subheadingsData
