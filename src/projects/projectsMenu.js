9a4fabee902adbe8feb832d7c827ac3cdca42e8e
WilliamFoley <CaptainTrips1500@proton.me>
2025-01-06 11:06:34 -0500

    updated projectsMenu renderContent

--- src/projects/projectsMenu.js
+++ src/projects/projectsMenu.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react'
 
 export default class projectsMenu extends Component {
-  render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeProject: 1,
+        };
+    }
+
+    handleProjectClick = (project) => {
+        this.setState ({
+            activeProject: project,
+        });
+    };
+
+    renderContent = (projects) => {
+        return projects.map((project, index) => {
+            <div key={index} className={`project-sub-container-${index + 1}`}>
+                <h3>{project.title}</h3>
+                <img src={project.image} alt={project.title}/>
+                <div>{project.description}</div>
+                <div className="link-container">
+                    <a href={project.github} target="_blank" rel='noopener noreferrer'>GITHUB</a>
+                    <a href={project.demo} target="_blank" rel='noopener noreferrer'>DEMO</a>
+                </div>
+            </div>
+        })
+    }
+  
+    render() {
     return (
-      <div>projectsMenu</div>
+        <div className="project-menu">
+            <div className="project-item-container">
+                {project.Items.map((item, index) => (
+                    <div key={index} classname={classNames("project-item", {activeProject: activeProject === index})}></div>
+                ))}
+            </div>
+        </div>
     )
   }
 }
