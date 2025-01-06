f2bb01a36fd9f5c67931145d131817ebedb0640e
WilliamFoley <CaptainTrips1500@proton.me>
2025-01-06 11:28:25 -0500

    fixed additional errors in projectMenu and projectsData

--- src/projects/projectsMenu.js
+++ src/projects/projectsMenu.js
@@ -18,7 +18,7 @@ export default class projectsMenu extends Component {
     };
 
     renderContent = (projects) => {
-        return projects.map((project, index) => {
+        return projects.map((project, index) => (
             <div key={index} className={`project-sub-container-${index + 1}`}>
                 <h3>{project.title}</h3>
                 <img src={project.image} alt={project.title}/>
@@ -28,18 +28,18 @@ export default class projectsMenu extends Component {
                     <a href={project.demo} target="_blank" rel='noopener noreferrer'>DEMO</a>
                 </div>
             </div>
-        })
+        ))
     }
   
     render() {
         const {activeProject} = this.state;
-        const projectItem = ["PROJECT ONE", "PROJECT TWO", "PROJECT THREE"];
+        const projectItems = ["PROJECT ONE", "PROJECT TWO", "PROJECT THREE"];
 
         return (
             <div className="project-menu">
                 <div className="project-item-container">
-                    {projects.Items.map((item, index) => (
-                        <div key={index} classname={classNames("project-item", {activeProject: activeProject === index + 1})} onClick={() => this.handleProjectClick(index + 1)}>
+                    {projectItems.map((item, index) => (
+                        <div key={index} className={classNames("project-item", {activeProject: activeProject === index + 1})} onClick={() => this.handleProjectClick(index + 1)}>
                             <h2 className="title">{item}</h2>
                         </div>
                     ))}
